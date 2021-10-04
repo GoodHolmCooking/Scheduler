f6a7a4098b44b449b22d061f972a42124c519c99
Ryan Holm <rholm667@gmail.com>
2021-10-04 13:55:25 -0400

    Removed old, commented out code

--- src/Model/Authenticator.java
+++ src/Model/Authenticator.java
@@ -11,19 +11,6 @@ public class Authenticator {
     private final HashMap<String, Integer> ids = new HashMap<String, Integer>();
     private String currentUser = null;
 
-//    public boolean authenticate(String suppliedUser, String suppliedPassword) {
-//        boolean passwordCorrect = false;
-//        if (passwords.containsKey(suppliedUser)) {
-//            String correctPassword = passwords.get(suppliedUser);
-//            if (suppliedPassword.equals(correctPassword)) {
-//                passwordCorrect = true;
-//                this.currentUser = suppliedUser;
-//            }
-//        }
-//
-//        return passwordCorrect;
-//    }
-
     public HashMap<String, String> getPasswords() {
         return passwords;
     }
